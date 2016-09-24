faf0af442f9e95c534b61eb332aec1610418d961
= <nncl@live.com>
2016-09-23 21:19:03 -0300

    Adding provider

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,15 +1,18 @@
 import {Component} from '@angular/core';
 import {AboutPage} from '../about/about';
+import {PeopleService} from '../../providers/people-service/people-service';
 
 @Component({
-    templateUrl: 'build/pages/home/home.html'
+    templateUrl: 'build/pages/home/home.html',
+    providers: [PeopleService]
 })
 
 export class HomePage {
     public name;
+    public people: any;
     aboutPage = AboutPage;
 
-    constructor() {
+    constructor(public peopleService : PeopleService) {
         this.name = 'Cauê';
     }
 }
